9c7ef226e7a4a0efb2cfe1c62388d852263b1048
fallch0222 <56762890+fallch0222@users.noreply.github.com>
2022-01-27 23:12:57 +0900

    Update delay.cs

--- delay.cs
+++ delay.cs
@@ -1,14 +1,14 @@
 private static DateTime Delay(int MS) //delay
-        {
-            // More effective then Thread and Timer
-            DateTime ThisMoment = DateTime.Now;
-            TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
-            DateTime AfterWards = ThisMoment.Add(duration);
+{
+    // More effective then Thread and Timer
+    DateTime ThisMoment = DateTime.Now;
+    TimeSpan duration = new TimeSpan(0, 0, 0, 0, MS);
+    DateTime AfterWards = ThisMoment.Add(duration);
 
-            while (AfterWards >= ThisMoment)
-            {
+    while (AfterWards >= ThisMoment)
+    {
 
-                ThisMoment = DateTime.Now;
-            }
-            return DateTime.Now;
-        }
+        ThisMoment = DateTime.Now;
+    }
+    return DateTime.Now;
+}
